5f7474b1da6fb731837ecf391e9a9eb78aed4afb
nyakio <nyakio19roseary@gmail.com>
2025-12-06 00:03:18 +0300

    created base class for all dsp to provide unified interface

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -22,6 +22,7 @@ Project13AudioProcessor::Project13AudioProcessor()
                        )
 #endif
 {
+    
 }
 
 Project13AudioProcessor::~Project13AudioProcessor()
@@ -150,12 +151,12 @@ void Project13AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, ju
     // the samples and the outer loop is handling the channels.
     // Alternatively, you can process the samples with the channels
     // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
+    //for (int channel = 0; channel < totalNumInputChannels; ++channel)
+   // {
+       // auto* channelData = buffer.getWritePointer (channel);
 
         // ..do something to the data...
-    }
+   // }
 }
 
 //==============================================================================
