1e9dd7e35db6ce56dfbf02bf71df43c09e6cfe25
nyakio <nyakio19roseary@gmail.com>
2025-12-08 12:09:01 +0300

    Updated dsp from params

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -471,6 +471,29 @@ void Project13AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, ju
     //TODO:  delay module 
 
 
+    phaser.dsp.setRate(phaserRateHz->get());
+    phaser.dsp.setCentreFrequency(phaserCenterFreqHz->get());
+    phaser.dsp.setDepth(phaserDepthPercent->get());
+    phaser.dsp.setFeedback(phaserFeedbackPercent->get());
+    phaser.dsp.setMix(phaserMixPercent->get());
+
+    chorus.dsp.setRate(chorusRateHz->get());
+    chorus.dsp.setDepth(chorusDepthPercent->get());
+    chorus.dsp.setCentreDelay(chorusCenterDelayMs->get());
+    chorus.dsp.setFeedback(chorusFeedbackPercent->get());
+    chorus.dsp.setMix(chorusMixPercent->get());
+
+    overdrive.dsp.setDrive(overdriveSaturation->get());
+
+    ladderFilter.dsp.setMode(
+        static_cast<juce::dsp::LadderFilterMode>(ladderFilterMode->getIndex()));
+    ladderFilter.dsp.setCutoffFrequencyHz(ladderFilterCutoffHz->get());
+    ladderFilter.dsp.setResonance(ladderFilterCutoffHz->get());
+    ladderFilter.dsp.setDrive(ladderFilterDrive->get());
+
+
+
+
     auto newDSPOrder = DSP_Order();
 
     while (dspOrderFifo.pull(newDSPOrder)) 
